ef9bfbbf6b6a63a4ed2bb3dddea5e3bbd766ea85
extricate <hsfnelissen@gmail.com>
2018-01-30 21:09:15 +0100

    Still deciding on a design for the stats. Haven't even started on the actual implementation, heh.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -50,7 +50,7 @@
             <div class="col-md-4">
                 <div class="panel panel-default">
                     <div class="panel-heading">Your stats</div>
-                    <div class="panel-body text-right">
+                    <div class="panel-body">
                         @include('user.stats')
                     </div>
                 </div>
--- resources/views/user/stats.blade.php
+++ resources/views/user/stats.blade.php
@@ -1,55 +1,62 @@
-<div class="btn-group btn-group-stats" role="group" aria-label="...">
-    <div class="btn btn-strength">Strength: {{ $user->strength }}</div>
-    @if ($user->unallocated_stats > 0)
-        <button type="button" class="btn btn-default">
-            {{ Form::open(['class' => 'form-inline form-stats', 'method' => 'PUT', 'route' => ['allocate_stats', $stat = 'strength']]) }}
-            {{ Form::submit('up', ['class' => 'btn-stats-up']) }}
-            {{ Form::close() }}
-        </button>
-    @endif
-</div>
-<div class="btn-group btn-group-stats" role="group" aria-label="...">
-    <div class="btn btn-dexterity">Dexterity: {{ $user->dexterity }}</div>
-    @if ($user->unallocated_stats > 0)
-        <button type="button" class="btn btn-default">
-            {{ Form::open(['class' => 'form-inline form-stats', 'method' => 'PUT', 'route' => ['allocate_stats', $stat = 'dexterity']]) }}
-            {{ Form::submit('up', ['class' => 'btn-stats-up']) }}
-            {{ Form::close() }}
-        </button>
-    @endif
-</div>
-<div class="btn-group btn-group-stats" role="group" aria-label="...">
-    <div class="btn btn-intelligence">Intelligence: {{ $user->intelligence }}</div>
-    @if ($user->unallocated_stats > 0)
-        <button type="button" class="btn btn-default">
-            {{ Form::open(['class' => 'form-inline form-stats', 'method' => 'PUT', 'route' => ['allocate_stats', $stat = 'intelligence']]) }}
-            {{ Form::submit('up', ['class' => 'btn-stats-up']) }}
-            {{ Form::close() }}
-        </button>
-    @endif
-</div>
-<div class="btn-group btn-group-stats" role="group" aria-label="...">
-    <div class="btn btn-stamina">Stamina: {{ $user->stamina }}</div>
-    @if ($user->unallocated_stats > 0)
-        <button type="button" class="btn btn-default">
-            {{ Form::open(['class' => 'form-inline form-stats', 'method' => 'PUT', 'route' => ['allocate_stats', $stat = 'stamina']]) }}
-            {{ Form::submit('up', ['class' => 'btn-stats-up']) }}
-            {{ Form::close() }}
-        </button>
-    @endif
-</div>
-<div class="btn-group btn-group-stats" role="group" aria-label="...">
-    <div class="btn btn-charisma">Charisma: {{ $user->charisma }}</div>
-    @if ($user->unallocated_stats > 0)
-        <button type="button" class="btn btn-default">
-            {{ Form::open(['class' => 'form-inline form-stats', 'method' => 'PUT', 'route' => ['allocate_stats', $stat = 'charisma']]) }}
-            {{ Form::submit('up', ['class' => 'btn-stats-up']) }}
-            {{ Form::close() }}
-        </button>
-    @endif
-</div>
-@if ($user->unallocated_stats > 0)
-    <div class="btn-group btn-group-stats" role="group">
-        <div class="btn btn-unallocated">Unallocated: {{ $user->unallocated_stats }}</div>
+<div class="row">
+    <div class="col-md-4">
+
     </div>
-@endif
+    <div class="col-md-8 text-right">
+        <div class="btn-group btn-group-stats" role="group" aria-label="...">
+            <div class="btn btn-strength">Strength: {{ $user->strength }}</div>
+            @if ($user->unallocated_stats > 0)
+                <button type="button" class="btn btn-default btn-up">
+                    {{ Form::open(['class' => 'form-inline form-stats', 'method' => 'PUT', 'route' => ['allocate_stats', $stat = 'strength']]) }}
+                    {{ Form::submit('up', ['class' => 'btn-stats-up']) }}
+                    {{ Form::close() }}
+                </button>
+            @endif
+        </div>
+        <div class="btn-group btn-group-stats" role="group" aria-label="...">
+            <div class="btn btn-dexterity">Dexterity: {{ $user->dexterity }}</div>
+            @if ($user->unallocated_stats > 0)
+                <button type="button" class="btn btn-default btn-up">
+                    {{ Form::open(['class' => 'form-inline form-stats', 'method' => 'PUT', 'route' => ['allocate_stats', $stat = 'dexterity']]) }}
+                    {{ Form::submit('up', ['class' => 'btn-stats-up']) }}
+                    {{ Form::close() }}
+                </button>
+            @endif
+        </div>
+        <div class="btn-group btn-group-stats" role="group" aria-label="...">
+            <div class="btn btn-intelligence">Intelligence: {{ $user->intelligence }}</div>
+            @if ($user->unallocated_stats > 0)
+                <button type="button" class="btn btn-default btn-up">
+                    {{ Form::open(['class' => 'form-inline form-stats', 'method' => 'PUT', 'route' => ['allocate_stats', $stat = 'intelligence']]) }}
+                    {{ Form::submit('up', ['class' => 'btn-stats-up']) }}
+                    {{ Form::close() }}
+                </button>
+            @endif
+        </div>
+        <div class="btn-group btn-group-stats" role="group" aria-label="...">
+            <div class="btn btn-stamina">Stamina: {{ $user->stamina }}</div>
+            @if ($user->unallocated_stats > 0)
+                <button type="button" class="btn btn-default btn-up">
+                    {{ Form::open(['class' => 'form-inline form-stats', 'method' => 'PUT', 'route' => ['allocate_stats', $stat = 'stamina']]) }}
+                    {{ Form::submit('up', ['class' => 'btn-stats-up']) }}
+                    {{ Form::close() }}
+                </button>
+            @endif
+        </div>
+        <div class="btn-group btn-group-stats" role="group" aria-label="...">
+            <div class="btn btn-charisma">Charisma: {{ $user->charisma }}</div>
+            @if ($user->unallocated_stats > 0)
+                <button type="button" class="btn btn-default btn-up">
+                    {{ Form::open(['class' => 'form-inline form-stats', 'method' => 'PUT', 'route' => ['allocate_stats', $stat = 'charisma']]) }}
+                    {{ Form::submit('up', ['class' => 'btn-stats-up']) }}
+                    {{ Form::close() }}
+                </button>
+            @endif
+        </div>
+        @if ($user->unallocated_stats > 0)
+            <div class="btn-group btn-group-stats" role="group">
+                <div class="btn btn-unallocated">Unallocated: {{ $user->unallocated_stats }}</div>
+            </div>
+        @endif
+    </div>
+</div>
