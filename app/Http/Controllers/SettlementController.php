4ec7da2363fa09c9ff798c3893c68bba34d19322
extricate <hsfnelissen@gmail.com>
2018-01-31 12:37:38 +0100

    Combat Status middleware works now

--- app/Http/Controllers/SettlementController.php
+++ app/Http/Controllers/SettlementController.php
@@ -17,6 +17,7 @@ class SettlementController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+        $this->middleware('combat.status');
     }
 
     public function index()
--- app/Http/Middleware/CombatStatus.php
+++ app/Http/Middleware/CombatStatus.php
@@ -17,8 +17,8 @@ class CombatStatus
     public function handle($request, Closure $next)
     {
         // Check if the user is in combat, if so redirect to the combat page
-        $user_is_in_combat = Auth::user()->user_is_in_combat;
-        if ($user_is_in_combat == true) {
+        $user = Auth::user();
+        if ($user->is_in_combat == true) {
             return redirect(route('view_combat'))->with('message', 'Perhaps you should finish the fight first, captain?');
         } else {
             return $next($request);
