f702b471628ee73dd6d2e831c9cf2d7348fdc44e
extricate <hsfnelissen@gmail.com>
2018-02-02 16:35:36 +0100

    Multiple layouts for settlements

--- resources/views/settlement/index.blade.php
+++ resources/views/settlement/index.blade.php
@@ -10,18 +10,100 @@
                     <div class="panel-heading">
                         You are currently in the {{ $settlement->type }} "{{ $settlement->name }}"
                     </div>
-                    <div class="panel-body">
+
+
+                    @if ($settlement->type == 'Monastery')
 
                         <div class="settlement">
-                            <div class="settlement-icon settlement-primary">@svg('village', 'icon-xxxl')</div>
-                            <div class="settlement-icon settlement-icon-1">@svg('mansion', 'icon-xxxl')</div>
-                            <div class="settlement-icon settlement-icon-4">@svg('palm-tree', 'icon-xxxl')</div>
-                            <div class="settlement-icon settlement-icon-2">@svg('fort', 'icon-xxxl')</div>
+                            <div class="settlement-icon settlement-floor"></div>
+                            <div class="settlement-icon settlement-sky"></div>
+                            <div class="settlement-icon settlement-primary settlement-centered">@svg('church',
+                                'icon-xxxl')
+                            </div>
+                            <div class="settlement-icon settlement-icon-4">@svg('palm-tree', 'icon-xxl')</div>
+                            <div class="settlement-icon settlement-icon-7">@svg('palm-tree-inverse', 'icon-xxxl')</div>
+                        </div>
+
+                    @elseif ($settlement->type == 'Haven')
+                        <div class="settlement">
+                            <div class="settlement-icon settlement-floor"></div>
+                            <div class="settlement-icon settlement-sky"></div>
+                            <div class="settlement-icon settlement-icon-4">@svg('palm-island', 'icon-xxxl')</div>
+                            <div class="settlement-icon settlement-icon-3">@svg('hut', 'icon-xxxl')</div>
+                            <div class="settlement-icon settlement-icon-5">@svg('palm-island', 'icon-xxxl')</div>
+                            <div class="settlement-icon settlement-icon-bottom-left">@svg('bonfire', 'icon-xxl')</div>
+                            <div class="settlement-icon settlement-icon-6">@svg('palm-tree-inverse', 'icon-xxxl')</div>
+                        </div>
+                    @elseif ($settlement->type == 'Outpost')
+                        <div class="settlement">
+                            <div class="settlement-icon settlement-floor"></div>
+                            <div class="settlement-icon settlement-sky"></div>
+                            <div class="settlement-icon settlement-icon-4">@svg('palm-island', 'icon-xxxl')</div>
                             <div class="settlement-icon settlement-icon-3">@svg('village', 'icon-xxxl')</div>
+                            <div class="settlement-icon settlement-icon-bottom-right">@svg('well-sm', 'icon-xxl')</div>
+                            <div class="settlement-icon settlement-icon-bottom-left">@svg('bonfire', 'icon-xxl')</div>
+                            <div class="settlement-icon settlement-icon-6">@svg('palm-tree-inverse', 'icon-xxxl')</div>
+                        </div>
+
+                    @elseif ($settlement->type == 'Village')
+                        <div class="settlement">
+                            <div class="settlement-icon settlement-floor"></div>
+                            <div class="settlement-icon settlement-sky"></div>
+                            <div class="settlement-icon settlement-icon-6">@svg('village', 'icon-xxl')</div>
+                            <div class="settlement-icon settlement-icon-4">@svg('palm-island', 'icon-xxxl')</div>
+                            <div class="settlement-icon settlement-icon-3">@svg('village', 'icon-xxxl')</div>
+                            <div class="settlement-icon settlement-icon-top-right">@svg('village', 'icon-xxl')</div>
+                            <div class="settlement-icon settlement-icon-top-right">@svg('palm-tree-inverse', 'icon-xxl')</div>
+                            <div class="settlement-icon settlement-icon-bottom-right">@svg('well-sm', 'icon-xxl')</div>
+                            <div class="settlement-icon settlement-icon-bottom-left">@svg('bonfire', 'icon-xxl')</div>
+                            <div class="settlement-icon settlement-icon-6">@svg('palm-tree', 'icon-xxxl')</div>
+                        </div>
+                    @elseif ($settlement->type == 'Town')
+                        <div class="settlement">
+                            <div class="settlement-icon settlement-floor"></div>
+                            <div class="settlement-icon settlement-sky"></div>
+                            <div class="settlement-icon settlement-icon-6">@svg('villa', 'icon-xxl')</div>
+                            <div class="settlement-icon settlement-icon-top-left">@svg('palm-tree', 'icon-xl')</div>
+                            <div class="settlement-icon settlement-icon-center-left">@svg('village', 'icon-xxl')</div>
+                            <div class="settlement-icon settlement-icon-top-right">@svg('village', 'icon-xxl')</div>
+                            <div class="settlement-icon settlement-icon-top-right">@svg('palm-tree-inverse', 'icon-xxl')</div>
+                            <div class="settlement-icon settlement-icon-6">@svg('palm-tree', 'icon-xxxl')</div>
+                            <div class="settlement-icon settlement-icon-3">@svg('villa', 'icon-xxxl')</div>
+                            <div class="settlement-icon settlement-icon-bottom-right">@svg('well', 'icon-xxl')</div>
+                        </div>
+
+                    @elseif ($settlement->type == 'Metropolis')
+                        <div class="settlement">
+                            <div class="settlement-icon settlement-floor"></div>
+                            <div class="settlement-icon settlement-sky"></div>
+                            <div class="settlement-icon settlement-primary">@svg('villa-blue', 'icon-xxxl')</div>
+                            <div class="settlement-icon settlement-icon-top-center">@svg('mansion', 'icon-xxl')</div>
+                            <div class="settlement-icon settlement-icon-4">@svg('palm-island', 'icon-xxxl')</div>
+                            <div class="settlement-icon settlement-icon-2">@svg('fort', 'icon-xxxl')</div>
+                            <div class="settlement-icon settlement-icon-top-right">@svg('villa', 'icon-xxl')</div>
+                            <div class="settlement-icon settlement-icon-3">@svg('villa', 'icon-xxxl')</div>
+                            <div class="settlement-icon settlement-icon-bottom-right">@svg('well', 'icon-xxl')</div>
                             <div class="settlement-icon settlement-icon-5">@svg('palm-tree', 'icon-xxxl')</div>
                             <div class="settlement-icon settlement-icon-6">@svg('palm-tree-inverse', 'icon-xxxl')</div>
                         </div>
-                    </div>
+                    @elseif ($settlement->type == 'City')
+                        <div class="settlement">
+                            <div class="settlement-icon settlement-floor"></div>
+                            <div class="settlement-icon settlement-sky"></div>
+                            <div class="settlement-icon settlement-icon-top-left">@svg('villa', 'icon-xxxl')</div>
+                            <div class="settlement-icon settlement-icon-6">@svg('village', 'icon-xxxl')</div>
+                            <div class="settlement-icon settlement-icon-top-center">@svg('mansion', 'icon-xxl')</div>
+                            <div class="settlement-icon settlement-icon-4">@svg('palm-island', 'icon-xxxl')</div>
+                            <div class="settlement-icon settlement-icon-center-left">@svg('fort', 'icon-xxxl')</div>
+                            <div class="settlement-icon settlement-icon-top-right">@svg('fort', 'icon-xxxl')</div>
+                            <div class="settlement-icon settlement-icon-3">@svg('villa', 'icon-xxxl')</div>
+                            <div class="settlement-icon settlement-icon-bottom-right">@svg('well', 'icon-xxl')</div>
+                            <div class="settlement-icon settlement-icon-5">@svg('palm-tree', 'icon-xxxl')</div>
+                            <div class="settlement-icon settlement-icon-bottom-center">@svg('palm-tree-inverse', 'icon-xxxl')</div>
+                        </div>
+                    @elseif ($settlement->type == 'Capital')
+
+                    @endif
                 </div>
             </div>
             <div class="col-md-6">
